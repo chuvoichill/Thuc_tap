159c95a2dd6830487d946f9fe6357e62361dac08
hieu <hieuokchua@gmail.com>
2025-11-11 08:33:44 +0700

    Thay đổi bên hội sinh viên: 	modified:   frontend/src/components/drl/HSVStudentList.jsx 	modified:   frontend/src/components/drl/HSVStudentRow.jsx 	modified:   frontend/src/services/drlService.js

--- frontend/src/services/drlService.js
+++ frontend/src/services/drlService.js
@@ -50,10 +50,11 @@ export const getHSVClassStudents = (class_code, term) => {
   return api.get(`/api/hsv/class-students?class_code=${encodeURIComponent(class_code)}&term=${encodeURIComponent(term)}`);
 };
 
-export const confirmHSVAssessment = (student_code, term_code, participated, note, username) => {
+export const confirmHSVAssessment = (student_code, term_code, criterion_code, participated, note, username) => {
   return api.post('/api/hsv/confirm', {
     student_code,
     term_code,
+    criterion_code,
     participated,
     note,
     username
