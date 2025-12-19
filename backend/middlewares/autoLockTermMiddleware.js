b15c315b6be4f191db67429c54d55ac511ddca40
Van Duy <duy.vnua.cntt@gmail.com>
2025-12-19 23:04:57 +0700

    fix

--- backend/middlewares/autoLockTermMiddleware.js
+++ backend/middlewares/autoLockTermMiddleware.js
@@ -2,17 +2,18 @@ import { putLockTerm } from "../models/termModel.js";
 
 let lastRun = 0;
 
-export const autoLockTerm = async (req, res, next) => {
+export const autoLockTerm = async (req, res,next) => {
   try {
     const dateNow = Date.now();
 
     // Chạy mỗi 10 phút
     if (dateNow - lastRun > 10 * 60 * 1000) {
       await putLockTerm();
-      lastRun = now;
+      lastRun = dateNow;
     }
 
   } catch (errorr) {
     console.error("Lỗi ở ", errorr);
   }
+  next();
 };
